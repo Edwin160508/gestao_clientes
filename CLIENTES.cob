58406333f963eb55f21a915faa5786b01d132ed6
Edwin Lima <edwin.limaa@gmail.com>
2020-09-28 14:27:46 -0300

    Corrigindo bug, Move to WRK-MODULO.

--- CLIENTES.cob
+++ CLIENTES.cob
@@ -1,8 +1,8 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. GESTAO_CLIENTES.
+       PROGRAM-ID.  CLIENTES.
       ******************************************************************
-      * OBJETIVO: SISTEMA DE GESTAO DE CLIENTES
-      * AUTHOR: EDWIN PEREIRA OLIVEIRA LIMA
+      * OBJETIVO:  SISTEMA DE GESTAO DE CLIENTES
+      * AUTHOR  :  EDWIN PEREIRA OLIVEIRA LIMA
       ******************************************************************
       ******************************************************************
        ENVIRONMENT DIVISION.
@@ -13,80 +13,78 @@
       *-----------------------------------------------------------------
        WORKING-STORAGE SECTION.
       *-----------------------------------------------------------------
-       77 WRK-OPCAO PIC X(1).
+       77 WRK-OPCAO  PIC X(1).
        77 WRK-TITULO PIC X(29) VALUE 'SISTEMA DE GESTAO DE CLIENTES'.
        77 WRK-MODULO PIC X(25).
        77 WRK-TECLA PIC X(1).
 
-      *TELA DO SISTEMA SESSAO "SCREEN SECTION"
       *-----------------------------------------------------------------
        SCREEN SECTION.
       *-----------------------------------------------------------------
        01 TELA.
-           05 LIMPA_TELA.
-               10 BLANK SCREEN.
+            05 LIMPA-TELA.
+                10 BLANK SCREEN.
       * COMANDO PARA LIMPAR ATE O FINAL DA LINA (END 0F LINE) EOL
       * PIC X(20) ESTA DEFININDO A TAMNAHO DA TEXTO DA BARRA 20 POSICOES
-               10 LINE 01 COLUMN 01 PIC X(29) ERASE EOL
+                10 LINE 01 COLUMN 01 PIC X(29) ERASE EOL
       * PROPRIEDADE PARA COR DE FUNDO DA BARRA
                    BACKGROUND-COLOR 3.
       * PROPRIEDADE PARA COR DE FUNDO DO TEXTO TITULO FORCANDO TER MESMA COR DA BARRA
-               10 LINE 01 COLUMN 45 FOREGROUND-COLOR 0 FROM WRK-TITULO
-               BACKGROUND-COLOR 3.
+                10 LINE 01 COLUMN 45 PIC X(29)
+                   BACKGROUND-COLOR 3  FOREGROUND-COLOR 0
+                              FROM WRK-TITULO.
 
       * PROPRIEDADES DA BARRA DE MODULOS
-               10 LINE 02 COLUMN 01 PIC X(25) ERASE EOL
-               BACKGROUND-COLOR 1 FROM WRK-MODULO.
+                10 LINE 02 COLUMN 01 PIC X(25) ERASE EOL
+                   BACKGROUND-COLOR 1 FROM WRK-MODULO.
 
 
       * OPCOES DE MENU
        01 MENU.
-           05 LINE 07 COLUMN 15 VALUE '1 - INCLUIR'.
-           05 LINE 08 COLUMN 15 VALUE '2 - CONSULTAR'.
-           05 LINE 09 COLUMN 15 VALUE '3 - ALTERAR'.
-           05 LINE 10 COLUMN 15 VALUE '4 - EXCLUIR'.
-           05 LINE 11 COLUMN 15 VALUE '5 - RELATORIO'.
-           05 LINE 12 COLUMN 15 VALUE 'X - SAIR'.
-           05 LINE 13 COLUMN 15 VALUE'OPCAO......: '.
-           05 LINE 13 COLUMN 28 USING WRK-OPCAO.
-
-
+            05 LINE 07 COLUMN 15 VALUE '1 - INCLUIR'.
+            05 LINE 08 COLUMN 15 VALUE '2 - CONSULTAR'.
+            05 LINE 09 COLUMN 15 VALUE '3 - ALTERAR'.
+            05 LINE 10 COLUMN 15 VALUE '4 - EXCLUIR'.
+            05 LINE 11 COLUMN 15 VALUE '5 - RELATORIO'.
+            05 LINE 12 COLUMN 15 VALUE 'X - SAIR'.
+            05 LINE 13 COLUMN 15 VALUE 'OPCAO......: ' .
+            05 LINE 13 COLUMN 28 USING WRK-OPCAO.
       ******************************************************************
        PROCEDURE DIVISION.
       ******************************************************************
        0001-PRINCIPAL SECTION.
-           PERFORM 1000-INICIAR.
-           PERFORM 2000-PROCESSAR.
-           PERFORM 3000-FINALIZAR.
-           STOP RUN.
+            PERFORM 1000-INICIAR.
+            PERFORM 2000-PROCESSAR.
+            PERFORM 3000-FINALIZAR.
+            STOP RUN.
 
        1000-INICIAR.
-           DISPLAY TELA.
-           ACCEPT MENU.
+            DISPLAY TELA.
+            ACCEPT MENU.
        2000-PROCESSAR.
       * SWITCH DO COBOL VERIFICA QUAL OPCAO FOI ESCOLHIDA PELO USUARIO.
-           EVALUATE WRK-OPCAO
-               WHEN 1
-                   PERFORM 5000-INCLUIR
-               WHEN 2
-                   CONTINUE
-               WHEN 3
-                   CONTINUE
-               WHEN 4
-                   CONTINUE
-               WHEN 5
-                   CONTINUE
-               WHEN OTHER
-                   IF WRK-OPCAO NOT EQUAL 'X'
-                       DISPLAY 'OPCAO INVALIDA ESCOLHA OPCAO CORRETA.'
-                   END-IF
-           END-EVALUATE.
+            EVALUATE WRK-OPCAO
+              WHEN 1
+               PERFORM 5000-INCLUIR
+              WHEN 2
+                CONTINUE
+              WHEN 3
+                CONTINUE
+              WHEN 4
+                CONTINUE
+              WHEN 5
+                CONTINUE
+              WHEN OTHER
+                IF WRK-OPCAO NOT EQUAL 'X'
+                    DISPLAY 'ENTRE COM OPCAO CORRETA'
+                END-IF
+            END-EVALUATE.
 
 
        3000-FINALIZAR.
-           CONTINUE.
+             CONTINUE.
 
        5000-INCLUIR.
-           MOVE 'MODULO - INCLUSAO ' TO WRK-OPCAO.
-           DISPLAY TELA.
-           ACCEPT WRK-TECLA AT 1620.
+             MOVE 'MODULO - INCLUSAO ' TO WRK-MODULO.
+             DISPLAY TELA.
+             ACCEPT WRK-TECLA AT 1620.
